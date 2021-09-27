75b0dfb0684c3f0c56f7758e85161da2a463beec
Moitair <gustavosmelo05@gmail.com>
2021-09-27 11:41:14 -0300

    ufa finalmente foi já fiz a base

--- login.php
+++ login.php
@@ -1,7 +1,19 @@
 <?php
 
-$conn = mysqli_connect("localhost","root","","tb_usuario");
+$conn = mysqli_connect("localhost", "root", "", "banco");
 
 $usuario = $_POST['usuario'];
-$senha = $_POST['senha'];
+$senha = md5($_POST['senha']);
 
+$sqlinsert = "INSERT INTO tb_usuario(usuario,senha)
+VALUES('$usuario','$senha')";
+
+mysqli_query($conn, $sqlinsert) or die("Não foi possível inserir os dados");
+
+//Mostra os dados inseridos
+
+echo "Usuario inserido <br> Usuario: $usuario";
+
+echo "<br>";
+
+echo "Senha inserida <br> Senha: $senha";
